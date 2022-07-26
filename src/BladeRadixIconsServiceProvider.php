2323f415d8c3ec99a30a0f36dc6e0323bec4a58c
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2022-07-26 05:24:24 +0000

    Fix styling

--- src/BladeRadixIconsServiceProvider.php
+++ src/BladeRadixIconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeRadixIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-radix-icons', []);
 
-            $factory->add('radix-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('radix-icons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-radix-icons.php', 'blade-radix-icons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-radix-icons.php', 'blade-radix-icons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-radix-icons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-radix-icons'),
             ], 'blade-radix-icons');
 
             $this->publishes([
-                __DIR__.'/../config/blade-radix-icons.php' => $this->app->configPath('blade-radix-icons.php'),
+                __DIR__ . '/../config/blade-radix-icons.php' => $this->app->configPath('blade-radix-icons.php'),
             ], 'blade-radix-icons-config');
         }
     }
